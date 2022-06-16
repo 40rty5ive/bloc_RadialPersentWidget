d11eec89264e9536a1f0d7a799c1c6ed497c3b73
Yaroslav <apolisuk065@gmail.com>
2022-06-16 13:18:39 +0300

    first commit

---
+++ lib/widget.dart
@@ -0,0 +1,167 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class Example extends StatefulWidget {
+  const Example({Key? key}) : super(key: key);
+
+  @override
+  _ExampleState createState() => _ExampleState();
+}
+
+class _ExampleState extends State<Example> {
+  double _currentSliderValue = 0.5;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            width: 100,
+            height: 100,
+            child: RadialPersentWidget(
+              percent: _currentSliderValue,
+              fillColor: Color.fromRGBO(28, 34, 86, 1.0),
+              lineColor: Color.fromRGBO(161, 233, 97, 1.0),
+              freeColor: Color.fromRGBO(56, 80, 72, 1.0),
+              lineWidth: 8,
+              child: Text(
+                '${(_currentSliderValue * 100).toStringAsFixed(0)}%',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+          Slider(
+            value: _currentSliderValue,
+            max: 1,
+            onChanged: (double value) {
+              setState(() {
+                _currentSliderValue = value;
+              });
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class RadialPersentWidget extends StatelessWidget {
+  final Widget child;
+
+  final double percent;
+  final Color fillColor;
+  final Color lineColor;
+  final Color freeColor;
+  final double lineWidth;
+
+  const RadialPersentWidget({
+    Key? key,
+    required this.child,
+    required this.percent,
+    required this.fillColor,
+    required this.lineColor,
+    required this.freeColor,
+    required this.lineWidth,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        CustomPaint(
+            painter: MyPainter(
+          percent: percent,
+          fillColor: fillColor,
+          lineColor: lineColor,
+          freeColor: freeColor,
+          lineWidth: lineWidth,
+        )),
+        Padding(
+          padding: const EdgeInsets.all(11.0),
+          child: Center(
+            child: child,
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class MyPainter extends CustomPainter {
+  final double percent;
+  final Color fillColor;
+  final Color lineColor;
+  final Color freeColor;
+  final double lineWidth;
+
+  MyPainter({
+    required this.percent,
+    required this.fillColor,
+    required this.lineColor,
+    required this.freeColor,
+    required this.lineWidth,
+  });
+  @override
+  void paint(Canvas canvas, Size size) {
+    final Rect arcRect = calculateArcsRect(size);
+    drawBackground(canvas, size);
+    drawFreeArc(canvas, arcRect);
+    drawFilledArc(canvas, arcRect);
+  }
+
+  void drawFilledArc(Canvas canvas, Rect arcRect) {
+    final paint = Paint();
+    paint.color = lineColor;
+    paint.style = PaintingStyle.stroke;
+    paint.strokeWidth = lineWidth;
+    paint.strokeCap = StrokeCap.round;
+    canvas.drawArc(
+      arcRect,
+      -pi / 2,
+      pi * 2 * percent,
+      false,
+      paint,
+    );
+  }
+
+  void drawFreeArc(Canvas canvas, Rect arcRect) {
+    final paint = Paint();
+    paint.color = freeColor;
+    paint.style = PaintingStyle.stroke;
+    paint.strokeWidth = lineWidth;
+    canvas.drawArc(
+      arcRect,
+      pi * 2 * percent - (pi / 2),
+      pi * 2 * (1.0 - percent),
+      false,
+      paint,
+    );
+  }
+
+  void drawBackground(Canvas canvas, Size size) {
+    final paint = Paint();
+    paint.color = fillColor;
+    paint.style = PaintingStyle.fill;
+    canvas.drawOval(Offset.zero & size, paint);
+  }
+
+  Rect calculateArcsRect(Size size) {
+    final lineMargin = 3;
+    final offset = lineWidth / 2 + lineMargin;
+    final arcRect = Offset(offset, offset) &
+        Size(
+          size.width - offset * 2,
+          size.height - offset * 2,
+        );
+    return arcRect;
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
